bbd55f396103db9ca0506e293705f647ee809c58
zhxi <376178259@QQ.com>
2017-09-05 19:15:53 +0800

    EXCE-DEV

---
+++ EXCEL-DEV/EXCEL-DEV/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace EXCEL_DEV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.excel = new DevExpress.XtraSpreadsheet.SpreadsheetControl();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSet);
+            this.groupBox1.Controls.Add(this.btn);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1028, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.excel);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1028, 482);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(129, 32);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "读取";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // excel
+            // 
+            this.excel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.excel.Location = new System.Drawing.Point(3, 17);
+            this.excel.Name = "excel";
+            this.excel.Size = new System.Drawing.Size(1022, 462);
+            this.excel.TabIndex = 0;
+            this.excel.Text = "spreadsheetControl1";
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(261, 32);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(75, 23);
+            this.btnSet.TabIndex = 1;
+            this.btnSet.Text = "设置";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 582);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private DevExpress.XtraSpreadsheet.SpreadsheetControl excel;
+        private System.Windows.Forms.Button btnSet;
+    }
+}
+
---
+++ EXCEL-DEV/EXCEL-DEV/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Spreadsheet;
+using System.Diagnostics;
+
+namespace EXCEL_DEV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = string.Format(@"{0}\DATA\单元格.xlsx",Application.StartupPath);
+            excel.LoadDocument(path);
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            IWorkbook book = excel.Document;
+            Worksheet sht = book.Worksheets[0];
+            for (int i = 0; i < 10;i++ )
+            {
+                Cell cell = sht.Cells[i, 0];
+                Debug.Print("{0}:{1}",cell.DisplayText,cell.NumberFormat);
+
+            }
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            IWorkbook book = excel.Document;
+            Worksheet sht = book.Worksheets[0];
+            for (int i = 0; i < 10; i++)
+            {
+                Cell cell = sht.Cells[i, 0];
+                cell.NumberFormat = "0.000";
+                Debug.Print("{0}:{1}", cell.DisplayText, cell.NumberFormat);
+
+            }
+        }
+    }
+}
